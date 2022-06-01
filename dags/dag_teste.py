f641463c0d87f5a5e03437c8b8440354c17a8bdb
RafaelFOl <rafael_ferreira1994@hotmail.com>
2022-05-31 21:32:22 -0300

    k8s cluster init

---
+++ dags/dag_teste.py
@@ -0,0 +1,24 @@
+import pendulum
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
+with DAG(
+    dag_id="example_python_operator",
+    schedule_interval=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["example"],
+) as dag:
+
+    def print_array():
+        """Print Numpy array."""
+        a = np.arange(15).reshape(3, 5)
+        print(a)
+        return a
+
+    run_this = PythonOperator(
+        task_id="print_the_context",
+        python_callable=print_array,
+    )
