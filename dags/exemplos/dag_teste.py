5a17c2c458a4ad67e3fb09d2c5523561540ea4b5
RafaelFOl <rafael_ferreira1994@hotmail.com>
2022-06-08 00:31:16 -0300

    teste11

--- dags/exemplos/dag_teste.py
+++ dags/exemplos/dag_teste.py
@@ -23,10 +23,10 @@ with DAG(
         get_logs=True,
         cmds=['/opt/spark/bin/spark-submit'],
         arguments=[
-        '--master=k8s://https://10.96.0.1:443',
+        '"--master k8s://https://10.96.0.1:443"',
         '--deploy-mode cluster',
         '--name spark-pi',
-        ' --class org.apache.spark.examples.SparkPi',
+        '--class org.apache.spark.examples.SparkPi',
         '--conf spark.executor.instances=5',
         '--conf spark.kubernetes.container.image=senior2017/taxi-pipe:1.8',
         'local:///app/taxi-spark.py']
