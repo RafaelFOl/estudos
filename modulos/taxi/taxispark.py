bff1ce5f22be7457ee4b6cd544a2b71fe337519f
RafaelFOl <rafael_ferreira1994@hotmail.com>
2022-06-08 11:55:17 -0300

    teste25

--- modulos/taxi/taxispark.py
+++ modulos/taxi/taxispark.py
@@ -1,33 +1,19 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
+ 
 
+sparkConf = SparkConf()
+sparkConf.setMaster("k8s://https://10.96.0.1:443")
+sparkConf.setAppName("KUBERNETES-IS-AWESOME")
+sparkConf.set("spark.kubernetes.container.image", "itayb/spark:3.1.1-hadoop-3.2.0-aws")
+sparkConf.set("spark.kubernetes.namespace", "spark")
+sparkConf.set("spark.hadoop.fs.s3a.endpoint", "http://10.96.101.36:9000")
+sparkConf.set("spark.hadoop.fs.s3a.access.key", "myaccesskey")
+sparkConf.set("spark.hadoop.fs.s3a.secret.key", "mysecretkey")
 
-def run_taxi_pipe():
-    
-    conf=(SparkConf()
-         .setMaster("k8s://https://10.96.0.1:443")
-         .set('spark.hadoop.fs.s3a.endpoint','http://10.96.101.36:9000')
-         .set('spark.hadoop.fs.s3a.access.key','myaccesskey')
-         .set('spark.hadoop.fs.s3a.secret.key','mysecretkey')
-         .set('spark.hadoop.fs.s3a.path.style.access',True)
-         .set('spark.hadoop.fs.s3a.fast.upload',True)
-         .set('spark.hadoop.fs.s3a.connection.maximum',100)
-         .set('spark.hadoop.fs.s3a.impl','org.apache.hadoop.fs.s3a.S3AFileSystem')
-         .set('spark.delta.logStore.class','org.apache.spark.sql.delta.storage.S3SingleDriverLogStore')
-         .set('spark.sql.extensions','io.delta.sql.DeltaSparkSessionsExtension')
-         .set('spark.sql.catalog.spark_catalog','org.apache.spark.sql.delta.catalog.DeltaCatalog'))
-    sc=SparkContext(conf=conf).getOrCreate()
+spark = SparkSession.builder.config(conf=sparkConf).getOrCreate()
+sc = spark.sparkContext
 
-    
-    spark=SparkSession.builder.appName('test-dag-airflow-spark').getOrCreate()
-    
-    #spark.SparkContext.setLogLevel("INFO")
+df = spark.read.json('s3a://estudos/credentials.json')
 
-    schema=spark.read.format('json').option('inferSchema','true').json('s3a://estudos/*json')
-
-    schema.show()
-
-    schema.printSchema()
-
-    spark.stop()
-    
+df.show()
