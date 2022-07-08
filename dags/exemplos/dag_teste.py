dd64e1bb093fa0f323b77ddc0b35f7cd01adfbd9
RafaelFOl <rafael_ferreira1994@hotmail.com>
2022-07-08 12:09:41 -0300

    teste

--- dags/exemplos/dag_teste.py
+++ dags/exemplos/dag_teste.py
@@ -5,9 +5,9 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import Kubernete
 from airflow.operators.python import PythonOperator
 from airflow.decorators import task,dag
 
-
-def _extract(ds):
-    print(ds)
+@task.python
+def soprintar(vb):
+    print(vb)
 
 @task.python
 def _xxcom():
@@ -21,13 +21,37 @@ with DAG(
     tags=["example"],
 ) as dag:
     
+    taxi_task_select = KubernetesPodOperator(
+        namespace='spark',
+        image="rroliveira/taxi-pipe:1.0",
+        name='taxi_task_select',
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="taxi_task_select",
+        get_logs=True,    
+        cmds=['/opt/spark/bin/spark-submit'],
+         arguments=[ 
+        '--master','k8s://https://10.96.0.1:443',
+        '--name','spark-name1',
+        '--conf', 'spark.kubernetes.namespace=spark',
+        '--conf', 'spark.kubernetes.allocation.batch.size=3',
+        '--conf', 'spark.kubernetes.allocation.batch.delay=1',
+        '--conf', 'spark.driver.cores=1',
+        '--conf', 'spark.executor.cores=1',
+        '--conf', 'spark.driver.memory=2192m',
+        '--conf', 'spark.executor.memory=2192m',
+        '--conf', 'spark.dynamicAllocation.enabled=true',
+        '--conf', 'spark.dynamicAllocation.shuffleTracking.enabled=true',
+        '--conf', 'spark.kubernetes.driver.container.image=rroliveira/taxi-pipe:1.0',
+        '--conf', 'spark.kubernetes.executor.container.image=rroliveira/taxi-pipe:1.0',
+        '--conf', 'spark.kubernetes.authenticate.driver.serviceAccountName=default',
+        '--class' ,'org.apache.spark.examples.SparkPi',
+        '--deploy-mode','cluster', 
+        'local:///app/taxispark.py'
+         ]
+     ) 
 
-
-    ds_teste = PythonOperator(
-        task_id="ds_teste",
-        python_callable=_extract
-    )
-    _xxcom()>>ds_teste
+    soprintar(_xxcom())
 
 
     
